f67beec8c2efad4f14c4a260655b68e7507746b9
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-27 18:15:30 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
-  validates :category, includsion: in:%w(Fiction Non-Fiction)
+  validates :category, includsion: {in: %w(Fiction Non-Fiction)}
   validate :title_include, on: :create
 
   TITLE_MATCH_ARRAY = [/Won't Believe/, /Secret/, /Top [0..99]/, /guess/]
