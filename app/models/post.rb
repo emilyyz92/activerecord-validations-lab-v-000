c6fe4e136c56aac953299db8082c6b7f6e93ba7f
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-27 18:11:28 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,11 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, includsion: in:%w(Fiction Non-Fiction)
-
+  validate :title,
 
   TITLE_MATCH_ARRAY = [/Won't Believe/, /Secret/, /Top [0..99]/, /guess/]
 
-  def title_include?
+  def title_include
     if TITLE_MATCH_ARRAY.none? {|a| title.include?(a)}
       errors.add(:title, "Titile is not sufficiently
       clickbait-y")
